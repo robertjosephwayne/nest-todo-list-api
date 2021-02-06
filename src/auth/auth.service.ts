682993fb5826997c63e13cec7acdc30a3d9ce5bc
Robert <robertjwayne@gmail.com>
2021-02-06 02:28:53 -0800

    Create initial version of AuthService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import * as bcrypt from 'bcrypt';
+import { UsersRepository } from 'src/users/repositories/users.repository';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly usersRepository: UsersRepository,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async validateUser(username: string, password: string): Promise<any> {
+    const user = await this.usersRepository.findByUsername(username);
+    const isPasswordMatching = await bcrypt.compare(password, user.password);
+    if (user && isPasswordMatching) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+
+  async login(user: any) {
+    const payload = { username: user.username, sub: user.userId };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+
+  async register(createUserDto: CreateUserDto) {
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+    const createdUser = await this.usersRepository.create({
+      ...createUserDto,
+      password: hashedPassword,
+    });
+    const { password, ...result } = createdUser;
+    return result;
+  }
+}
