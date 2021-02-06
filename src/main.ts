02a6be677fe7986212c63c66f7376c3c5d281fef
Robert <robertjwayne@gmail.com>
2021-02-06 02:30:00 -0800

    Use helmet, rateLimit, enable CORS

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
+import * as csurf from 'csurf';
+import * as rateLimit from 'express-rate-limit';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Set security-related HTTP headers
+  app.use(helmet());
+
+  // CSRF protection
+  // app.use(csurf());
+
+  // Rate limiting
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 100, // limit each IP to 100 requests per windowMs
+    }),
+  );
+
+  // CORS
+  app.enableCors();
+
   await app.listen(3000);
 }
 bootstrap();
