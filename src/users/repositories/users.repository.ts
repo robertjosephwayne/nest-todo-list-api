728a601b02bd0ea55b8feec76f6679458100752b
Robert <robertjwayne@gmail.com>
2021-02-06 02:30:35 -0800

    Create initial version of UsersRepository

---
+++ src/users/repositories/users.repository.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateUserDto } from '../dto/create-user.dto';
+import { User, UserDocument } from '../schemas/user.schema';
+
+@Injectable()
+export class UsersRepository {
+  constructor(
+    @InjectModel(User.name)
+    private userModel: Model<UserDocument>,
+  ) {}
+
+  async findAll(): Promise<User[]> {
+    return this.userModel.find().exec();
+  }
+
+  async findById(id: string): Promise<User> {
+    return this.userModel.findById(id);
+  }
+
+  async findByUsername(username: string): Promise<User> {
+    return this.userModel.findOne({ username });
+  }
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const createdUser = new this.userModel(createUserDto);
+    return createdUser.save();
+  }
+}
