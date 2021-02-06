585971358b42d164c85e8c8654e8de811440ee5d
Robert <robertjwayne@gmail.com>
2021-02-06 02:27:57 -0800

    Create login and signup routes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,52 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+import { LocalAuthGuard } from './auth/guards/local-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly authService: AuthService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(
+    @Body('username') username: string,
+    @Body('password') password: string,
+  ) {
+    const user = { username, password };
+    console.log('Logging in: ', user);
+    return this.authService.login(user);
+  }
+
+  @Post('auth/signup')
+  async signup(
+    @Body('username') username: string,
+    @Body('password') password: string,
+  ) {
+    const user = { username, password };
+    console.log('Registering user: ', user);
+    return this.authService.register(user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
   }
 }
