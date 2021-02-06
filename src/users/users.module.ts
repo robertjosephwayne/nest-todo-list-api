5ee1883787d627a481826c3e79670b61b4f2fa9f
Robert <robertjwayne@gmail.com>
2021-02-06 02:31:04 -0800

    Create initial version of Users Module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './schemas/user.schema';
+import { UsersController } from './controllers/users.controller';
+import { UsersRepository } from './repositories/users.repository';
 
 @Module({
-  providers: [UsersService]
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
+  providers: [UsersRepository],
+  controllers: [UsersController],
+  exports: [MongooseModule, UsersRepository],
 })
 export class UsersModule {}
