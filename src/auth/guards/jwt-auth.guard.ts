070f521fe3be2daedbfabe947a931d9250e6edfb
Robert <robertjwayne@gmail.com>
2021-02-07 02:51:04 -0800

    Temporarily comment out public decorator functionality

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -5,18 +5,19 @@ import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
-  constructor(private reflector: Reflector) {
-    super();
-  }
-
-  canActivate(context: ExecutionContext) {
-    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
-      context.getHandler(),
-      context.getClass(),
-    ]);
-    if (isPublic) {
-      return true;
-    }
-    return super.canActivate(context);
-  }
+  // constructor(private reflector: Reflector) {
+  //   super();
+  // }
+  // canActivate(context: ExecutionContext) {
+  //   console.log('JwtAuthGuard canActivate');
+  //   console.log('context: ', context);
+  //   const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+  //     context.getHandler(),
+  //     context.getClass(),
+  //   ]);
+  //   if (isPublic) {
+  //     return true;
+  //   }
+  //   return super.canActivate(context);
+  // }
 }
