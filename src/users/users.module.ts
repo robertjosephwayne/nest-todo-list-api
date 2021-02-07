b6bcc9d158b5b6a0cc1c692930b49c64ebba9d2a
Robert <robertjwayne@gmail.com>
2021-02-07 02:47:32 -0800

    Import ProjectsModule, remove MongooseModule from exports

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,13 +3,16 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schemas/user.schema';
 import { UsersController } from './controllers/users.controller';
 import { UsersRepository } from './repositories/users.repository';
+import { ProjectsRepository } from 'src/projects/repositories/projects.repository';
+import { ProjectsModule } from 'src/projects/projects.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    ProjectsModule,
   ],
   providers: [UsersRepository],
   controllers: [UsersController],
-  exports: [MongooseModule, UsersRepository],
+  exports: [UsersRepository],
 })
 export class UsersModule {}
