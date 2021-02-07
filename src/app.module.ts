e76cb13993837e62dd67221d2828151e9197a3a4
Robert <robertjwayne@gmail.com>
2021-02-07 02:46:28 -0800

    Add ProjectsController, TodosController, ProjectsModule, TodosModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,10 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { ProjectsController } from './projects/controllers/projects.controller';
+import { ProjectsModule } from './projects/projects.module';
+import { TodosModule } from './todos/todos.module';
+import { TodosController } from './todos/controllers/todos.controller';
 
 @Module({
   imports: [
@@ -14,8 +18,10 @@ import { ConfigModule } from '@nestjs/config';
       port: process.env.MONGO_PORT,
     }),
     UsersModule,
+    ProjectsModule,
+    TodosModule,
   ],
-  controllers: [AppController],
+  controllers: [AppController, ProjectsController, TodosController],
   providers: [AppService],
 })
 export class AppModule {}
