f258a8a89ebdaf313e825044b699da41fd113c40
Robert <robertjwayne@gmail.com>
2021-02-08 00:08:00 -0800

    Delete all todos when a project is deleted

--- src/projects/repositories/projects.repository.ts
+++ src/projects/repositories/projects.repository.ts
@@ -3,16 +3,21 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Project, ProjectDocument } from '../schemas/project.schema';
 import * as mongoose from 'mongoose';
+import { TodoDocument } from 'src/todos/schemas/todo.schema';
+import { CreateProjectDto } from '../dto/create-project.dto';
+import { UpdateProjectDto } from '../dto/update-project.dto';
 
 @Injectable()
 export class ProjectsRepository {
   constructor(
     @InjectModel('Project')
     private projectModel: Model<ProjectDocument>,
+    @InjectModel('Todo')
+    private todoModel: Model<TodoDocument>,
   ) {}
 
-  async create(project: Partial<Project>): Promise<Project> {
-    const newProject = new this.projectModel(project);
+  async create(createProjectDto: CreateProjectDto): Promise<Project> {
+    const newProject = new this.projectModel(createProjectDto);
     await newProject.save();
     return newProject.toObject({ versionKey: false });
   }
@@ -29,10 +34,18 @@ export class ProjectsRepository {
         },
       },
     ]);
-    // return this.projectModel.find({ user: userId });
   }
 
   async findOne(id): Promise<Project> {
     return this.projectModel.findById(id);
+  }
+
+  async remove(id: string) {
+    await this.todoModel.deleteMany({ project: id });
+    return this.projectModel.deleteOne({ _id: id });
+  }
+
+  update(id: string, updateProjectDto: UpdateProjectDto) {
+    return this.projectModel.updateOne({ _id: id }, updateProjectDto);
   }
 }
