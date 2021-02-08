091f3e3447f6176ce353683f641c80a25d0fb942
Robert <robertjwayne@gmail.com>
2021-02-08 00:06:36 -0800

    Add ConfigModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,9 +5,11 @@ import { LocalStrategy } from './strategies/local.strategy';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './strategies/jwt.strategy';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     UsersModule,
     PassportModule,
     JwtModule.register({
