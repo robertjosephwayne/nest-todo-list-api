abb1e02a91644170a03d74dc90c8682fc76a0e17
Robert <robertjwayne@gmail.com>
2021-02-08 00:09:03 -0800

    Add Project to MongooseModule import

--- src/todos/todos.module.ts
+++ src/todos/todos.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProjectsModule } from 'src/projects/projects.module';
+import { Project, ProjectSchema } from 'src/projects/schemas/project.schema';
 import { TodosController } from './controllers/todos.controller';
 import { TodosRepository } from './repositories/todos.repository';
 import { Todo, TodoSchema } from './schemas/todo.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Todo.name, schema: TodoSchema }]),
+    MongooseModule.forFeature([
+      { name: Todo.name, schema: TodoSchema },
+      { name: Project.name, schema: ProjectSchema },
+    ]),
     ProjectsModule,
   ],
   controllers: [TodosController],
