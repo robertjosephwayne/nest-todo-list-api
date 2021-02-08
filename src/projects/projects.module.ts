7486bc12e0eb9d5803b887acc716c169bdf9e1c5
Robert <robertjwayne@gmail.com>
2021-02-08 00:07:45 -0800

    Add Todo to MongooseModule import

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from 'src/auth/auth.module';
+import { Todo, TodoSchema } from 'src/todos/schemas/todo.schema';
 import { ProjectsController } from './controllers/projects.controller';
 import { ProjectsRepository } from './repositories/projects.repository';
 import { Project, ProjectSchema } from './schemas/project.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Project.name, schema: ProjectSchema }]),
+    MongooseModule.forFeature([
+      { name: Project.name, schema: ProjectSchema },
+      { name: Todo.name, schema: TodoSchema },
+    ]),
   ],
   controllers: [ProjectsController],
   providers: [ProjectsRepository],
