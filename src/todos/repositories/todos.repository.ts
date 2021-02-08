6e2943adc3a4a665aa63a5062193809c320e8df3
Robert <robertjwayne@gmail.com>
2021-02-08 00:08:52 -0800

    Add update functionality and removeByProjectId

--- src/todos/repositories/todos.repository.ts
+++ src/todos/repositories/todos.repository.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { ProjectsRepository } from 'src/projects/repositories/projects.repository';
+import { ProjectDocument } from 'src/projects/schemas/project.schema';
 import { CreateTodoDto } from '../dto/create-todo.dto';
 import { UpdateTodoDto } from '../dto/update-todo.dto';
-import { TodoDocument } from '../schemas/todo.schema';
+import { Todo, TodoDocument } from '../schemas/todo.schema';
 
 @Injectable()
 export class TodosRepository {
   constructor(
     @InjectModel('Todo')
     private todoModel: Model<TodoDocument>,
+    @InjectModel('Project')
+    private projectModel: Model<ProjectDocument>,
   ) {}
 
   async create(createTodoDto: CreateTodoDto) {
@@ -22,26 +26,32 @@ export class TodosRepository {
     return newTodo.toObject({ versionKey: false });
   }
 
-  async findByUserId(userId: string) {
+  async findByUserId(userId: string): Promise<TodoDocument[]> {
     return this.todoModel.find({ user: userId });
   }
 
-  findAll() {
-    // TODO
-    return `This action returns all todos`;
+  async findOne(id: string) {
+    return this.todoModel.findById(id);
   }
 
-  findOne(id: number) {
-    // TODO
-    return `This action returns a #${id} todo`;
-  }
-
-  update(id: number, updateTodoDto: UpdateTodoDto) {
-    // TODO
-    return `This action updates a #${id} todo`;
+  async update(id: string, updateTodoDto: UpdateTodoDto) {
+    const foundProject = await this.projectModel.findById(
+      updateTodoDto.project,
+    );
+    return this.todoModel.findOneAndUpdate(
+      { _id: id },
+      {
+        ...updateTodoDto,
+        project: foundProject,
+      },
+    );
   }
 
   remove(id: string) {
     return this.todoModel.deleteOne({ _id: id });
+  }
+
+  removeByProjectId(projectId: string) {
+    return this.todoModel.deleteMany({ project: projectId });
   }
 }
